d1580e1cc83e3761a7e974224c4be0424118d47e
gax <gax@disroot.org>
2023-03-24 19:23:24 +0000

    Actualizar 'clienteMqtt/clienteMqtt.py'
    ensure_closed()

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -44,5 +44,5 @@ async def main():
                         logging.error(traceback.format_exc())
                         # Logs the error appropriately. 
 
- if __name__ == "__main__":
+if __name__ == "__main__":
     asyncio.run(main())
