73fff0ca4aba008add96040c117ee4793be0334a
Germán Xander <german.xander@fio.unam.edu.ar>
2024-04-15 16:08:50 -0300

    compartir conexión

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -3,6 +3,16 @@ import aiomqtt
 
 logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
 
+async def suscribir(client, topico):
+    await client.subscribe(topico)
+    async for message in client.messages:
+        logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+
+async def publicar(client, topico):
+    while True:
+        await asyncio.sleep(5)
+        await client.publish(topico, payload=2024)
+
 async def main():
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     tls_context.verify_mode = ssl.CERT_REQUIRED
@@ -14,9 +24,9 @@ async def main():
         port=8883,
         tls_context=tls_context,
     ) as client:
-        await client.subscribe(os.environ['TOPICO'])
-        async for message in client.messages:
-            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
-
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(publicar(client, os.environ['TOPICO_PUB']))
+            tg.create_task(suscribir(client, os.environ['TOPICO_SUB']))
+        
 if __name__ == "__main__":
     asyncio.run(main())
