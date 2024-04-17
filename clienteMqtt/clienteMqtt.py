631699656b848daab202fdc648d395e648be1ad8
Germán Xander <german.xander@fio.unam.edu.ar>
2024-04-17 08:54:20 -0300

    referencias

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -2,13 +2,13 @@ import asyncio, ssl, certifi, logging, os
 import aiomqtt
 import types
 
+# https://github.com/python/typeshed/pull/11609
 logging.basicConfig(format='%(asctime)s - task: %(taskName)s - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
 
 async def suscribir(client, topico):
     await client.subscribe(topico)
     async for message in client.messages:
         logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
-    await asyncio.sleep(0)
 
 async def publicar(client, topico, contador):
     while True:
@@ -23,6 +23,7 @@ async def contar(contador):
         await asyncio.sleep(3)
 
 async def main():
+    # https://stackoverflow.com/a/41765294
     contador = types.SimpleNamespace()
     contador.valor = 0
 
@@ -36,12 +37,14 @@ async def main():
         port = 8883,
         tls_context = tls_context,
     ) as client:
+        # https://sbtinstruments.github.io/aiomqtt/subscribing-to-a-topic.html#multiple-queues
         async with asyncio.TaskGroup() as tg:
             tg.create_task(publicar(client, os.environ['TOPICO_PUB'],contador), name='publicador')
-            tg.create_task(suscribir(client, os.environ['TOPICO_SUB']))
+            tg.create_task(suscribir(client, os.environ['TOPICO_SUB']), name='suscriptor')
             tg.create_task(contar(contador), name='contador')                
         
 if __name__ == "__main__":
+    # https://stackoverflow.com/questions/70399670/how-to-shutdown-gracefully-on-keyboard-interrupt-when-an-asyncio-task-is-perform
     try:
         asyncio.run(main())
     except KeyboardInterrupt as e:
